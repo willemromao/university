15d7abbe12d19cc680a370a8e65a70ab68d592c4
Willem Romão <franciscowillem@outlook.com>
2022-07-23 14:43:30 -0300

    Update 01.py

--- pensamento_computacional/condicionais/01.py
+++ pensamento_computacional/condicionais/01.py
@@ -1,13 +1,13 @@
-anodenascimento = int(input('Ano de nascimento: '))
+anodenascimento = int(input('Ano de nascimento: ')) #idade
 idade = 2022 - anodenascimento
 print(idade , "anos")
 
 if idade >= 16:
   print("\nPode votar")
 else:
-    print("Não pode votar")
+    print("NÃ£o pode votar")
 
 if idade >= 18:
   print("Pode ter CNH")
 else:
-  print("Não pode ter CNH")
+  print("NÃ£o pode ter CNH")
