4b45372618337fea399a23f5dba9b0f4324bc4b2
Willem Romão <franciscowillem@outlook.com>
2022-07-27 15:48:17 -0300

    Delete pensamento_computacional directory

--- pensamento_computacional/entradas-saidas-operadores/1.py
+++
@@ -1,4 +0,0 @@
-VL = float(input('Qual valor do litro de combustível? '))
-D = float(input('Quanto deseja abastecer R$? '))
-L = D / VL
-print("\nQuantidade em litros: {:.2f}".format(L), "L")
--- pensamento_computacional/entradas-saidas-operadores/2.py
+++
@@ -1,5 +0,0 @@
-KMi = float(input('Km incial: '))
-KMf = float(input('Km final: '))
-L = float(input('Litros gastos: '))
-CC = (KMf - KMi) / L
-print("\nA média de consumo de combustível do veículo é: ", CC, "L por Km")
