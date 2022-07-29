54f778939b35a1790e2ec4819f6e83d0b497b263
Willem Romão <franciscowillem@outlook.com>
2022-07-28 21:00:07 -0300

    Update 2.py

--- pensamento-computacional/entradas-saidas-operadores/2.py
+++ pensamento-computacional/entradas-saidas-operadores/2.py
@@ -1,5 +1,7 @@
 KMi = float(input('Km incial: '))
 KMf = float(input('Km final: '))
 L = float(input('Litros gastos: '))
+
 CC = (KMf - KMi) / L
-print("\nA média de consumo de combustível do veículo é: ", CC, "L por Km")
+
+print("\nA mÃ©dia de consumo de combustÃ­vel do veÃ­culo Ã©: ", CC, "L por Km")
