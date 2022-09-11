6ef9ef6ed71244ac6c4be68154bd0acf8550f94a
willemromao <franciscowillem@gmail.com>
2022-09-11 14:35:05 -0300

    _

---
+++ itp/aulas/aula03/exemplo2.c
@@ -0,0 +1,30 @@
+/* Faça uma calculadora simples que aceite as quatro operações: soma, subtração, multiplicação e divisão.
+O programa deve executar as contas até receber uma operação F, que indica o final do programa. 
+Cada linha de entrada do programa é realizada no formato NUMERO OPERADOR NUMERO. */
+
+#include <stdio.h>
+
+int main(){
+    
+    int n1, n2;
+    char op;
+
+    do{
+
+        printf("Digite a conta desejada (Valor OP Valor):\n");
+        scanf("%d %c %d", &n1, &op, &n2);
+
+        switch (op){
+            case '+': printf("%d\n", n1 + n2); break;
+            case '-': printf("%d\n", n1 - n2); break; 
+            case '*': printf("%d\n", n1 * n2); break; 
+            case '/':
+                if (n2 == 0) printf("Erro! Divisão por zero!\n");
+                else printf("%d\n", n1 / n2);
+            break; 
+        }
+
+    }while (op != 'F');
+
+    return 0;
+}
