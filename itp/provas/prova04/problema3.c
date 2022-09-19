e07d73b917b5f6ee331740ab9b65274203314eac
willemromao <franciscowillem@gmail.com>
2022-09-19 16:28:14 -0300

    _

--- itp/provas/prova04/problema3.c
+++ itp/provas/prova04/problema3.c
@@ -2,50 +2,51 @@
 
 int nperfeito(int X){
 
-int divisor, soma_divisores, i, nperfeito;
+    int soma_divisores = 0;
+    int i, nperfeito;
 
 //achar divisores
 
-    for(i=1; i<X, i++){
+    for(i=1; i<X; i++){
 
         if(X % i == 0){
-            divisor = i;
             soma_divisores = soma_divisores + i;
         }
     } 
 
-//testar se soma_divisores é = X
+//testar se soma_dividores é = X
     if(soma_divisores == X){
         nperfeito = 1;
     }
     else{
         nperfeito = 0;
     }
+    // printf("soma de %d= %d\n", X, soma_divisores);
 
     return nperfeito;
 }
 
 int main(){
 
-int N, X, j;
+int X, N, j;
     
     //recebe N casos
     scanf("%d", &N);
 
     //N = número de casos a serem verificados
 
-    for(j=0; j<=N; j++){
+    for(j=0; j<N; j++){
     //recebe X
         scanf("%d", &X);
+        //determinar se é perfeito
+        if(nperfeito(X) == 1){
+            printf("%d eh perfeito\n", X);
+        }
+        else{
+            printf("%d nao eh perfeito\n", X);
+        }
     }
 
-    //determinar se é perfeito
-    if(nperfeito(X) == 1){
-        printf("%d eh perfeito", X);
-    }
-    else(nperfeito(X) ==0){
-        printf("%d nao eh perfeito", X);
-    }
 
     return 0;
 }
