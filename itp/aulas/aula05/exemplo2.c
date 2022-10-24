8e679ae76892b5f34b209d5c9e1b35da0e6dd68d
willemromao <franciscowillem@gmail.com>
2022-10-24 19:57:17 -0300

    _

--- itp/aulas/aula05/exemplo2.c
+++ itp/aulas/aula05/exemplo2.c
@@ -32,11 +32,30 @@ int main () {
 
     printf("Soma das matrizes 1 e 2:\n");
     
-    for(i=0; i<TAM1; i++){
+    for(i=0; i<TAM1; i++) {
         for(j=0; j<TAM1; j++)
             printf("%d ", resultadoSoma[i][j]);
         printf("\n");    
     }
+
+
+    //Multiplicação da matriz 2 com 3
+
+    for(i=0; i<TAM1; i++)
+        for(j=0; j<TAM2; j++) {
+            resultadoMulti[i][j] = 0;
+            for(k=0; k<TAM1; k++)
+                resultadoMulti[i][j] += matriz2[i][k] * matriz3[k][j];
+        }
     
+    printf("Multiplicação da matrizes 2 e 3:\n");
+
+    for (i=0; i<TAM1; i++) { 
+        for(j=0; j<TAM2; j++)
+            printf("%d ", resultadoMulti[i][j]);
+        printf("\n");    
+    } 
+
+
     return 0;
 }
